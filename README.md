1a2043a666c4dfad95ebebaa2e98e19c06815464
srout <rout.sumanta@gmail.com>
2015-07-28 22:13:52 +0530

    Added Gestures

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # IonicExample
 
 https://www.youtube.com/watch?v=sCnGSOaaZFo
+
+http://codepen.io/shprink/pen/txliu
+
+http://codepen.io/calendee/pen/fbEsw
+
+https://public-api.wordpress.com/rest/v1.1/freshly-pressed/
+
+http://ionicframework.com/docs/api/directive/ionHeaderBar/
+
--- freshlystarted/platforms/ios/www/js/app.js
+++ freshlystarted/platforms/ios/www/js/app.js
@@ -2,9 +2,52 @@ var App = angular.module("App", ["ionic"]);
 
 App.service("FreshlyPressed", ["$http", "$log", FreshlyPressed]);
 
-App.controller("AppCtrl", ["$scope", "FreshlyPressed", "$log", AppCtrl]);
+App.controller("AppCtrl", ["$scope", "FreshlyPressed", "$log", "$ionicGesture", "$window", "$interval", AppCtrl]);
 
-function AppCtrl($scope, FreshlyPressed, $log) {
+//http://codepen.io/shprink/pen/txliu
+//http://codepen.io/calendee/pen/fbEsw
+
+function handleGesture($scope, $ionicGesture, $window, $interval) {
+    $scope.lastEventCalled = 'Try to Drag the content up, down, left or rigth';
+    var element = angular.element(document.querySelector('#eventPlaceholder'));
+    var events = [{
+                  event: 'dragup',
+                  text: 'You dragged me UP!'
+                  },{
+                  event: 'dragdown',
+                  text: 'You dragged me Down!'
+                  },{
+                  event: 'dragleft',
+                  text: 'You dragged me Left!'
+                  },{
+                  event: 'dragright',
+                  text: 'You dragged me Right!'
+                  }];
+    
+    angular.forEach(events, function(obj){
+                    $ionicGesture.on(obj.event, function (event) {
+                                     if(obj.event == 'dragup') {
+                                     alert("dragup");
+                                     }
+                                     if(obj.event == 'dragdown') {
+                                     alert("dragdown");
+                                     }
+                                     if(obj.event == 'dragleft') {
+                                     alert("dragleft");
+                                     }
+                                     if(obj.event == 'dragright') {
+                                     alert("dragright");
+                                     }
+                                     
+                                     $scope.$apply(function () {
+                                                   $scope.lastEventCalled = obj.text;
+                                                   });
+                                     }, element);
+                    });
+}
+
+function AppCtrl($scope, FreshlyPressed, $log, $ionicGesture, $window, $interval) {
+    handleGesture($scope, $ionicGesture, $window, $interval);
     $scope.posts = [];
     $scope.refresh = function() {
         FreshlyPressed.getBlogs($scope);
